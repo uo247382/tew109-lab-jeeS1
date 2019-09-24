af5c9e32e912ddc77571b68906b3d69d38658860
Fernando <fernando@LAPTOP-3AJ6CVFG>
2019-09-24 23:30:21 +0200

    Cargar el Proyecto

---
+++ src/ServletPrueba/ServletPrueba.java
@@ -0,0 +1,48 @@
+package ServletPrueba;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SerletPrueba
+ */
+@WebServlet("/ServletPrueba")
+public class ServletPrueba extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletPrueba() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("Bienvenido a mi primera Me lo cambia página web!");
+		out.println("</BODY></HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException, ServletException {
+		doGet(request,response );
+	}
+
+}
